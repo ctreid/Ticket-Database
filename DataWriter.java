07c6f13a1dbb4b5131c81def5d4004faf2c74bcc
jpbell411 <59890559+jpbell411@users.noreply.github.com>
2020-04-06 17:26:08 -0400

    Add files via upload

---
+++ DataWriter.java
@@ -0,0 +1,77 @@
+
+	import java.io.FileWriter;
+	import java.io.IOException;
+	import java.util.ArrayList;
+	import org.json.simple.JSONArray;
+	import org.json.simple.JSONObject;
+	public class DataWriter extends DataConstant {
+		public static void saveMovie() {
+			Movie movie= movie.getInstance();
+			JSONArray jsonMovie = new JSONArray();
+			
+			for(int i = 0; i < movie.size(); i++) {
+				jsonMovie.add(getMovieJSON(movie.get(i)));
+			}
+			
+	        try (FileWriter file = new FileWriter(MOVIE_FILE_NAME)) {
+	            file.write(jsonMovie.toJSONString());
+	            file.flush();
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+		public static JSONObject getMovieJSON(Movie movies) {
+			JSONObject movieDetails = new JSONObject();
+			movieDetails.put(MOVIE_TITLE, movies.getTitle());
+			movieDetails.put(MOVIE_COST_PER_TICKET, movies.getCostperTicket());
+			movieDetails.put(MOVIE_DURATION_TIME, movies.getDurationTime());
+			movieDetails.put(MOVIE_DESCRIPTION,movies.getDescription());
+	        return movieDetails;
+		}
+		public static void saveUser() {
+			User user= user.getInstance();
+			JSONArray jsonUser = new JSONArray();
+			
+			for(int i = 0; i < user.size(); i++) {
+				jsonUser.add(getUserJSON(user.get(i)));
+			}
+			
+	        try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+	            file.write(jsonUser.toJSONString());
+	            file.flush();
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+		public static JSONObject getUserJSON(User users) {
+			JSONObject userDetails = new JSONObject();
+			userDetails.put(USER_USERNAME, users.getuserName());
+			userDetails.put(USER_PASSWORD, users.getPassword());
+	        return userDetails;
+		}
+		
+		public static void saveTheater() {
+			Theater theater= theater.getInstance();
+			JSONArray jsonTheater = new JSONArray();
+			
+			for(int i = 0; i < theater.size(); i++) {
+				jsonTheater.add(getTheaterJSON(theater.get(i)));
+			}
+			
+	        try (FileWriter file = new FileWriter(THEATER_FILE_NAME)) {
+	            file.write(jsonTheater.toJSONString());
+	            file.flush();
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+		public static JSONObject getTheaterJSON(Theater theater) {
+			JSONObject theaterDetails = new JSONObject();
+			theaterDetails.put(USER_USERNAME, theater.getName());
+			theaterDetails.put(USER_PASSWORD, theater.getLocation());
+	        return theaterDetails;
+		}
+	}
