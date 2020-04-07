ac44402c95aa1df61aa7738de19c092e84ff7f43
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-04-07 08:37:07 -0400

    Update Movies.java

--- Movies.java
+++ Movies.java
@@ -2,7 +2,7 @@ package project;
 import java.util.ArrayList;
 
 public class Movies {
-	private static Movies movies;
+	/*private static Movies movies;
 	private ArrayList<Movie> movieList;
 	
 	private Movies() {
@@ -22,5 +22,30 @@ public class Movies {
 	
 	public Movie getMovie(String movieName) {
 		return null;
+	}*/
+	
+	private static Movies movies = null;
+	private static ArrayList<Movie> movieList = new ArrayList<Movie>();
+	
+	public Movies() {
+		movieList = DataLoader.loadMovie();
 	}
+	
+	public static Movies getInstance() {
+		if(movies == null) {
+			movies = new Movies();
+		}
+		
+		return movies;
+	}
+	
+	public ArrayList<Movie> getMovie() {
+		return movieList;
+	}
+	
+	public void addMovie(String title, double costPerTicket, String durationTime, String description) {
+		movieList.add(new Movie(title, costPerTicket, durationTime, description));
+		DataWriter.saveMovie();
+	}
+}
 }
