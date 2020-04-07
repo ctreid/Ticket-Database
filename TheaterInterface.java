7a6659ecd09b790716a61dc0ffcfa27c35613aee
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-04-07 17:22:02 -0400

    Update TheaterInterface.java

--- TheaterInterface.java
+++ TheaterInterface.java
@@ -1,30 +1,28 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class PlayInterface {
+public class TheaterInterface {
 	private Scanner scanner;
 	
-	public PlayInterface() {
+	public TheaterInterface() {
 		scanner = new Scanner (System.in);
 	}
 	
-	public void play() {
-		Theaters play = Theaters.getInstance();
+	public void theater() {
+		Theaters theater = Theaters.getInstance();
 		
 		System.out.println("******Your Movie Ticket******");
-		displayPlay();
+		displayTheater();
 		
-		while(addPlay()) {
-			String playTitle = getField("Movie Title");
-			String playCostPerTicket = getField("Movie Ticket Cost");
-			String playDurationTime = getField("Movie Duration");
-			String playDescription = getField("Movie Description");
+		while(addTheater()) {
+			String theaterName = getField("Theater Name");
+			String theaterLocation = getField("Theater Location");
 			
-			play.addPlay(playTitle, playCostPerTicket, playDurationTime, playDescription);
+			theater.addTheater(theaterName, theaterLocation);
 		}
 		
-		System.out.println("******Your updated play list******");
-		displayPlay();
+		System.out.println("******Your updated theater list******");
+		displayTheater();
 		
 		System.out.println("\nHave a good day!");
 	}
@@ -34,21 +32,20 @@ public class PlayInterface {
 		return scanner.nextLine();
 	}
 	
-	public boolean addPlay() {
-		System.out.print("Would you like to add a play? (Y or N): ");
+	public boolean addTheater() {
+		System.out.print("Would you like to add a theater? (Y or N): ");
 		
 		String input = scanner.nextLine();
 		
 		if(input.toLowerCase().trim().equals("y"))return true;
 		return false;
 	}
-	public void displayPlay() {
-		Plays play = Plays.getInstance();
-		ArrayList<Play> plays = play.getPlay();
+	public void displayTheater() {
+		Theaters theater = Theaters.getInstance();
+		ArrayList<Theater> theaters = theater.getTheater();
 		
-		for(Play p : plays) {
-			System.out.println(p.getTitle() + " " + p.getCostPerTicket() + " " + 
-					" " +  p.getDescription());
+		for(Theater t : theaters) {
+			System.out.println(t.getName() + " " + t.getLocation());
 		}
 	}
 }
