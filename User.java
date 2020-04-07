941bc04e021110ae3325c46e2cd27491ed3e09c4
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-04-07 08:39:31 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,5 +1,27 @@
-package project;
-
 public class User {
-
+	private String password;
+	private String userName;
+	
+	public User(String password, String userName) {
+		this.password = password;
+		this.userName = userName;
+	}
+	public User() {
+		
+	}
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
