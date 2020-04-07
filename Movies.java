2b0dfb000dadf88d50021e4f764b0084e1bd1d6b
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-04-07 17:20:43 -0400

    Update Movies.java

--- Movies.java
+++ Movies.java
@@ -1,34 +1,11 @@
-package project;
 import java.util.ArrayList;
-
 public class Movies {
-	/*private static Movies movies;
-	private ArrayList<Movie> movieList;
-	
-	private Movies() {
-		movieList = Database.getMovies();
-	}
-	
-	public static Movies getInstance() {
-		if (movies == null) {
-			movies = new Movies();
-		}
-		return movies;
-	}
-	
-	public boolean haveMovie(String movieNamee) {
-		return true;
-	}
-	
-	public Movie getMovie(String movieName) {
-		return null;
-	}*/
-	
 	private static Movies movies = null;
 	private static ArrayList<Movie> movieList = new ArrayList<Movie>();
 	
 	public Movies() {
 		movieList = DataLoader.loadMovie();
+		
 	}
 	
 	public static Movies getInstance() {
@@ -39,10 +16,6 @@ public class Movies {
 		return movies;
 	}
 	
-	public boolean haveMovie(String movieNamee) {
-		return true;
-	}
-	
 	public ArrayList<Movie> getMovie() {
 		return movieList;
 	}
@@ -51,5 +24,4 @@ public class Movies {
 		movieList.add(new Movie(title, costPerTicket, durationTime, description));
 		DataWriter.saveMovie();
 	}
-}
 }
