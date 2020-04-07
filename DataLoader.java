4002e2d68c3ee87bb837bb6cb9a7e94621fc2ff8
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-04-07 10:20:49 -0400

    Update DataLoader.java

--- DataLoader.java
+++ DataLoader.java
@@ -1,6 +1,6 @@
-
 import java.io.FileReader;
 import java.util.ArrayList;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;;
@@ -12,13 +12,13 @@ public class DataLoader extends DataConstant{
 		
 		try {
 			FileReader reader = new FileReader(USER_FILE_NAME);
-			JSONParser parser = new JSONParser();
+			//JSONParser parser = new JSONParser();
 			JSONArray userJSON = (JSONArray)new JSONParser().parse(reader);
 			
 			for(int i = 0; i < userJSON.size(); i++) {
-				JSONObject userJSON = (JSONObject)userJSON.get(i);
-				String userName  = (String)userJSON.get(USER_USERNAME);
-				String password = (String)userJSON.get(USER_PASSWORD);
+				JSONObject userJson = (JSONObject)userJSON.get(i);
+				String userName  = (String)userJson.get(USER_NAME);
+				String password = (String)userJson.get(USER_PASSWORD);
 				
 				user.add(new User(userName, password));
 			}
@@ -38,11 +38,11 @@ public class DataLoader extends DataConstant{
 		
 		try {
 			FileReader reader = new FileReader(USER_FILE_NAME);
-			JSONParser parser = new JSONParser();
+			//JSONParser parser = new JSONParser();
 			JSONArray theaterJson = (JSONArray)new JSONParser().parse(reader);
 		
-			for(int i = 0; i < theaterJSON.size(); i++) {
-				JSONObject theaterJSON = (JSONObject)theaterJSON.get(i);
+			for(int i = 0; i < theaterJson.size(); i++) {
+				JSONObject theaterJSON = (JSONObject)theaterJson.get(i);
 				String name  = (String)theaterJSON.get(THEATER_NAME);
 				String location = (String)theaterJSON.get(THEATER_LOCATION);
 				
@@ -63,17 +63,18 @@ public class DataLoader extends DataConstant{
 		
 		try {
 			FileReader reader = new FileReader(MOVIE_FILE_NAME);
-			JSONParser parser = new JSONParser();
-			JSONArray movieJSON = (JSONArray)new JSONParser().parse(reader);
+			//JSONParser parser = new JSONParser();
+			JSONArray movies = (JSONArray)new JSONParser().parse(reader);
 			
-			for(int i = 0; i < movieJSON.size(); i++) {
-				JSONObject movieJSON = (JSONObject)movieJSON1.get(i);
-				String title  = (String)movieJSON.get(MOVIE_TITLE);
-				int costperTicket = (int)movieJSON.get(MOVIE_COST_PER_TICKET);
-				int durationTime  = (int)movieJSON.get(MOVIE_DURATION_TIME);
-				String description = (String)movieJSON.get(MOVIE_DESCRIPTION);
+			for(int i = 0; i < movies.size(); i++) {
+				JSONObject movieJson = (JSONObject)movies.get(i);
+				String title  = (String)movieJson.get(MOVIE_TITLE);
 				
-				movie.add(new Movie(title,costperTicket,durationTime,description));
+				double costPerTicket = (double)movieJson.get(MOVIE_COST_PER_TICKET);
+				String durationTime  = (String)movieJson.get(MOVIE_DURATION_TIME);
+				String description = (String)movieJson.get(MOVIE_DESCRIPTION);
+				
+				movie.add(new Movie(title,costPerTicket,durationTime,description));
 			}
 			
 			return movie;
@@ -84,6 +85,7 @@ public class DataLoader extends DataConstant{
 		
 		return null;
 	}
+
 	public static ArrayList<Play> loadPlay() {
 		ArrayList<Play> play = new ArrayList<Play>();
 		
@@ -109,3 +111,11 @@ public class DataLoader extends DataConstant{
 		return null;
 	}
 }
+
+
+
+
+
+
+
+
