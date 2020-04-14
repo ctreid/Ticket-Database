2385e15620ad4665acd12d5be89482ab2cb938a3
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-04-13 20:51:57 -0400

    Add files via upload

---
+++ TheaterInterfaceTester.java
@@ -0,0 +1,13 @@
+
+public class TheaterInterfaceTester {
+
+	public static void main(String[] args) {
+		TheaterInterface theaterInter = new TheaterInterface();
+		System.out.println("***********TESTING THEATER INTERFACE CLASS***********");
+		
+		System.out.println("***********TESTING THEATER METHOD***********");
+		theaterInter.theater();
+
+	}
+
+}
